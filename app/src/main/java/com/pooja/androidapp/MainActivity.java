aadc81374b0c8d01322b9c84d83904bbe300cdc1
Bhargav Jani <bhargav.jani@einfochips.com>
2017-12-26 20:37:27 +0530

    first commit

---
+++ app/src/main/java/com/pooja/androidapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.pooja.androidapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void buttonClicked(View view) {
+        Log.i("info", "The button was clicked");
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
